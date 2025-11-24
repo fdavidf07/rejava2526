5fa7e9a6761c9a1d03cf0fc9af5ce030cb2d57a4
fdavidf07 <davicitofg@gmail.com>
2025-11-24 10:05:25 +0100

    Clase 24 nov

--- src/tema4/Electrodomestico.java
+++ src/tema4/Electrodomestico.java
@@ -2,48 +2,55 @@ package tema4;
 
 public class Electrodomestico {
 
-	String modelo, marca;
-	int dias;
-	double precio,potencia,horas,preEner;
-	
-	public Electrodomestico() {
-		this.modelo = "7200";
-		this.marca = "bosh";
-		this.dias = 3;
-		this.precio = 400;
-		this.potencia = 2300;
-		this.horas = 1;
-		this.preEner = 0.4039;
+	protected static final String MODELO_BASE="7200",COLOR_BASE="gris";
+	protected static final double PRECIO_BASE=400, POTENCIA_BASE=2300,PRECIO_ENERGIA_BASE=0.4039;
+	protected static final int HORAS_BASE= 1,DIAS_BASE=3;
+	public enum MARCA_BASE{
+		Bosh, Samsung, LG, Whirpol, Siemens
 	}
 	
-	public Electrodomestico(String modelo, String marca, int dias, double precio, double potencia, double horas,
+	private String sModel, sMarca,sColor;
+	protected int iDias,iHoras;
+	protected double dPrecioEnergia;
+	private double dPotencia,dPrecioElectrodom;
+		
+	//por defecto
+	public Electrodomestico() {
+		
+		this(MODELO_BASE,PRECIO_BASE,COLOR_BASE,POTENCIA_BASE,HORAS_BASE,DIAS_BASE,PRECIO_ENERGIA_BASE);
+	}
+	
+	public Electrodomestico(String modelo, double precio,String color, double potencia, int horas,int dias,
 			double preEner) {
 		super();
-		this.modelo = modelo;
-		this.marca = marca;
-		this.dias = dias;
-		this.precio = precio;
-		this.potencia = potencia;
-		this.horas = horas;
-		this.preEner = preEner;
+		this.sModel = modelo;
+		this.iDias = dias;
+		this.dPrecioEnergia = precio;
+		this.dPotencia = potencia;
+		this.iHoras = horas;
+		this.dPrecioElectrodom = preEner;
 	}
 	
-	public Electrodomestico(String modelo, String marca, double precio) {
-		super();
-		this.modelo = modelo;
-		this.marca = marca;
-		this.precio = precio;
+	public Electrodomestico(String paramModelo, double paramPrecio) {
+		this(paramModelo,paramPrecio,COLOR_BASE,POTENCIA_BASE,HORAS_BASE,DIAS_BASE,PRECIO_ENERGIA_BASE);
 	}
 
 
 
 	public double ConsumoAnual() {
-		double HorasAnyo= this.horas*3*52;// 7 dias semanales 52 semanas al año
-		double Consumo;
+		double HorasAnyo= this.iHoras*this.iDias*52;// 7 dias semanales 52 semanas al año
 		// horas * potencia entre 1000 * preEner
-		Consumo=(HorasAnyo *(this.potencia/1000)* this.preEner);
-		return Consumo;
+		return (HorasAnyo *(this.dPotencia/1000)* this.dPrecioElectrodom);
 	}
+
+	
+	@Override
+	public String toString() {
+		return "Electrodomestico [sModel=" + sModel + ", sMarca=" + sMarca + ", iDias=" + iDias + ", iHoras=" + iHoras
+				+ ", dPrecioEnergia=" + dPrecioEnergia + ", dPotencia=" + dPotencia + ", dPrecioElectrodom="
+				+ dPrecioElectrodom + "]";
+	}
+	
 	
 	}
 
---
+++ src/tema4/TelvisionTest.java
@@ -0,0 +1,9 @@
+package tema4;
+
+public class TelvisionTest {
+public static void main(String[] args) {
+
+	Television t=new Television();
+	System.out.print(t.consumoAnual());
+}
+}
