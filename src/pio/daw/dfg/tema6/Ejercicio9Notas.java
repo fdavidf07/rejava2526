78d778317f177ade5c9c9c1aa52939fda929f146
fdavidf07 <davicitofg@gmail.com>
2026-01-29 13:05:54 +0100

    .

--- src/pio/daw/dfg/tema6/Ejercicio9Notas.java
+++ src/pio/daw/dfg/tema6/Ejercicio9Notas.java
@@ -10,10 +10,10 @@ public class Ejercicio9Notas {
 	        String[] nombresAlumnos = {"Turing", "Lovelace", "Berners-Lee", "Gates"};
 	        String[] nombresAsignaturas = {"BBDD", "Programación", "Entornos", "Marcas", "Sostenibilidad"};
 
-	        // 1. Asignación aleatoria
-	    //    for (int i = 0; i < 4; i++) {
-	          //  for (int j = 0; j < 5; j++) {
-	           //     notas[i][j] = generarNotaAleatoria();
+	        /* 1. Asignación aleatoria
+	         for (int i = 0; i < 4; i++) {
+	            for (int j = 0; j < 5; j++) {
+	               notas[i][j] = generarNotaAleatoria();
 	                	
 	        }
 
@@ -21,17 +21,18 @@ public class Ejercicio9Notas {
 
 	        
 	        // 3. Cálculos estadísticos
-	/*        for (int i = 0; i < 4; i++) {
+	      for (int i = 0; i < 4; i++) {
 	            System.out.println("\nEstadísticas de " + nombresAlumnos[i] + ":");
 	            System.out.println("  Nota mínima: " + calcularMinima(notas[i]));
 	            System.out.println("  Nota máxima: " + calcularMaxima(notas[i]));
 	            System.out.println("  Nota media: " + calcularMedia(notas[i]));
 	        }
 	    }
-*/
+
 	private static double generarNotaAleatoria() {
 
 		double randomNumber = Math.random() * 10 + 1;
 		return randomNumber;
-	}
+	}*/
+}
 }
