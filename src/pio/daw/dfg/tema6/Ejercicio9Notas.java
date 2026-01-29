0a64fce249c987f5803a236de22828d5b7907d39
fdavidf07 <davicitofg@gmail.com>
2026-01-29 12:59:17 +0100

    ejercicio9

--- src/pio/daw/dfg/tema6/Ejercicio9Notas.java
+++ src/pio/daw/dfg/tema6/Ejercicio9Notas.java
@@ -1,5 +1,6 @@
 package pio.daw.dfg.tema6;
 
+import java.awt.ItemSelectable;
 import java.util.Arrays;
 
 public class Ejercicio9Notas {
@@ -10,29 +11,27 @@ public class Ejercicio9Notas {
 	        String[] nombresAsignaturas = {"BBDD", "Programación", "Entornos", "Marcas", "Sostenibilidad"};
 
 	        // 1. Asignación aleatoria
-	        for (int i = 0; i < 4; i++) {
-	            for (int j = 0; j < 5; j++) {
-	                notas[i][j] = generarNotaAleatoria();
-	            }
+	    //    for (int i = 0; i < 4; i++) {
+	          //  for (int j = 0; j < 5; j++) {
+	           //     notas[i][j] = generarNotaAleatoria();
+	                	
 	        }
 
 	        // 2. Mostrar las notas por consola
-	        xxxx
 
+	        
 	        // 3. Cálculos estadísticos
-	        for (int i = 0; i < 4; i++) {
+	/*        for (int i = 0; i < 4; i++) {
 	            System.out.println("\nEstadísticas de " + nombresAlumnos[i] + ":");
 	            System.out.println("  Nota mínima: " + calcularMinima(notas[i]));
 	            System.out.println("  Nota máxima: " + calcularMaxima(notas[i]));
 	            System.out.println("  Nota media: " + calcularMedia(notas[i]));
 	        }
 	    }
-
+*/
 	private static double generarNotaAleatoria() {
 
+		double randomNumber = Math.random() * 10 + 1;
+		return randomNumber;
 	}
-
-	// --- MÉTODOS de cálculos ---
-	double randomNumber = Math.random() * 10 + 1;
 }
-
